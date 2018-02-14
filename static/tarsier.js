131b579fdd13fb3267ef613c65ef8885a1cd028e
Fabio Viola <desmovalvo@gmail.com>
2018-02-14 13:35:13 +0100

    Added colours settings and enable/disable functionality for classes

--- static/tarsier.js
+++ static/tarsier.js
@@ -1,6 +1,7 @@
 // variables
 ws = null;
 scene = null;
+lastData = null;
 
 function sendRequest(serverUri){
 
@@ -27,6 +28,9 @@ function sendRequest(serverUri){
 	    console.log("[DEBUG] Connection ok");
 	    console.log(data);
 
+	    // store data
+	    lastData = data;
+
 	    // get all the tables
 	    opt = document.getElementById("objectPropertiesTable");
 	    dpt = document.getElementById("dataPropertiesTable");
@@ -37,19 +41,12 @@ function sendRequest(serverUri){
 		clt.deleteRow(-1);
 	    };
 	    for (c in data["classes"]){
+		cName = data["classes"][c];		
 		newRow = clt.insertRow(-1);
 		newCell = newRow.insertCell(0);
-		newCell.innerHTML = data["classes"][c];
-	    }
-
-	    // remove old classes and re-fill the table
-	    while(dpt.rows.length > 0) {
-		dpt.deleteRow(-1);
-	    };
-	    for (dp in data["properties"]["datatype"]){
-		newRow = dpt.insertRow(-1);
-		newCell = newRow.insertCell(0);
-		newCell.innerHTML = data["properties"]["datatype"][dp];
+		newCell.innerHTML = '<input type="checkbox" value="" id="' + cName + '_enabled" checked>'
+		newCell = newRow.insertCell(1);
+		newCell.innerHTML = cName;
 	    }
 	    
 	    // remove old data properties and re-fill the table
@@ -57,9 +54,12 @@ function sendRequest(serverUri){
 		dpt.deleteRow(-1);
 	    };
 	    for (dp in data["properties"]["datatype"]){
+		dpName = data["properties"]["datatype"][dp];
 		newRow = dpt.insertRow(-1);
 		newCell = newRow.insertCell(0);
-		newCell.innerHTML = data["properties"]["datatype"][dp];
+		newCell.innerHTML = '<input type="checkbox" value="" id="' + dpName + '_enabled">'
+		newCell = newRow.insertCell(1);
+		newCell.innerHTML = dpName;
 	    }
 
 	    // remove old object properties and re-fill the table
@@ -67,9 +67,12 @@ function sendRequest(serverUri){
 		opt.deleteRow(-1);
 	    };
 	    for (op in data["properties"]["object"]){
+		opName = data["properties"]["object"][op];
 		newRow = opt.insertRow(-1);
-		newCell = newRow.insertCell(0);
-		newCell.innerHTML = data["properties"]["object"][op];
+		newCell = newRow.insertCell(0);		
+		newCell.innerHTML = '<input type="checkbox" value="" id="' + opName + '_enabled">'
+		newCell = newRow.insertCell(1);
+		newCell.innerHTML = opName;
 	    }
 	    
 	    // console.log("Time to draw!");	   	    	    
@@ -110,6 +113,13 @@ function loadJSAP(){
 	    qURI = "http://" + myJson["parameters"]["host"] + ":" + myJson["parameters"]["ports"]["http"] + myJson["parameters"]["paths"]["query"];
 	    document.getElementById("queryUriInput").value = qURI;
 
+	    // retrieve colors
+	    console.log(myJson);
+	    console.log(myJson["extended"]["colors"]["classes"]);	   
+	    document.getElementById("classesColor").value = myJson["extended"]["colors"]["classes"];	   
+	    document.getElementById("datapropColor").value = myJson["extended"]["colors"]["dataProperties"];
+	    document.getElementById("objpropColor").value = myJson["extended"]["colors"]["objectProperties"];
+	    document.getElementById("instancesColor").value = myJson["extended"]["colors"]["instances"];
 	    
 	    // open the file
 
@@ -122,7 +132,7 @@ function loadJSAP(){
 };
 
 
-function draw(data){
+function draw_old(data){
 
     // get the canvas
     var canvas = document.getElementById('renderCanvas');
@@ -190,4 +200,82 @@ function draw(data){
     // 	engine.resize();
     // });
     
+}
+
+
+function draw(){
+
+    // get the canvas
+    var canvas = document.getElementById('renderCanvas');
+
+    // load the 3D engine
+    var engine = new BABYLON.Engine(canvas, true);
+
+    // createScene function that creates and return the scene
+    var createScene = function(){
+
+	// create a basic BJS Scene object
+	var scene = new BABYLON.Scene(engine);
+	scene.ambientColor = new BABYLON.Color3(1, 1, 1);
+	
+	// create a FreeCamera, and set its position (x,y,z)
+	var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2,  Math.PI / 4, 5, BABYLON.Vector3.Zero(), scene)
+	
+	// target the camera to scene origin
+	camera.setTarget(BABYLON.Vector3.Zero());
+	
+	// attach the camera to the canvas
+	camera.attachControl(canvas, false);
+	
+	// create a basic light, aiming 0,1,0 - meaning, to the sky
+	var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
+	   
+	// create a built-in "ground" shape;
+	var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
+
+	// draw classes
+	rgbColor = hexToRGB(document.getElementById("classesColor").value);
+	var orangeMat = new BABYLON.StandardMaterial("orangeMat", scene);
+	orangeMat.diffuseColor = new BABYLON.Color3(rgbColor[0], rgbColor[1], rgbColor[2]);	
+	nsize = Object.keys(lastData["classes"]).length;
+	node_angle = 360 / nsize;
+	
+	for (var k in lastData["classes"]){
+
+	    // check if it's enabled
+	    if (document.getElementById(lastData["classes"][k] + "_enabled").checked){	
+		var sphere = BABYLON.Mesh.CreateSphere(lastData["classes"][k], 16, 1, scene);
+		sphere.position.z = 5 * Math.sin(k*node_angle / 180*Math.PI);
+		sphere.position.x = 5 * Math.cos(k*node_angle / 180*Math.PI);
+		sphere.material = orangeMat;
+	    }
+	}
+	
+	// return the created scene
+	return scene;
+    }
+    
+    // call the createScene function
+    var scene = createScene();
+    
+    // run the render loop
+    engine.runRenderLoop(function(){
+	scene.render();
+    });
+
+}
+
+function cutHex(nStr) {
+    if (nStr.charAt(0) === "#") {
+	return nStr.substring(1, 7);
+    }
+    return nStr;
+};
+
+function hexToRGB(hexColor){
+
+    r = parseInt(cutHex(hexColor).substring(0,2),16).toFixed(2)/255;
+    g = parseInt(cutHex(hexColor).substring(2,4),16).toFixed(2)/255;
+    b = parseInt(cutHex(hexColor).substring(4,6),16).toFixed(2)/255;
+    return [r,g,b]
 }
